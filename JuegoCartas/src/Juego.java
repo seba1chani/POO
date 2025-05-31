36baea1c013574f6ac8696065dba2c9b8e428b59
Chañi Rodrigo Sebastian <131877586+seba1chani@users.noreply.github.com>
2025-05-31 00:49:46 -0300

    2.0.1

--- JuegoCartas/src/Juego.java
+++
@@ -1,5 +0,0 @@
-public class Juego {
-    public static void main(String[] args) {
-
-    }
-}
--- VentaLibros/src/MercadoPago.java
+++
@@ -1,5 +0,0 @@
-public class MercadoPago extends TipoDePago{
-    public double pagar(double monto){
-        return monto+monto*0.15;
-    }
-}
